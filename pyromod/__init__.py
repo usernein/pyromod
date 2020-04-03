9e5fb1cca1e5a99aba8021c19d83ee16448465f4
Cezar H <29507335+usernein@users.noreply.github.com>
2020-04-03 16:53:30 -0300

    0.0.45: Removing unrelated stuff

--- pyromod/__init__.py
+++ pyromod/__init__.py
@@ -18,4 +18,4 @@ You should have received a copy of the GNU General Public License
 along with pyromod.  If not, see <https://www.gnu.org/licenses/>.
 """
 
-__version__ = "0.0.4"
+__version__ = "0.0.45"
