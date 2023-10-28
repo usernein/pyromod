ab3ef9f1ca194fdd5cdca7e13cbcb1c2d48a9564
Pauxis <pauxiscezar@gmail.com>
2023-10-28 19:36:00 -0300

    chore!: remove handlers from pyromod exports

--- pyromod/__init__.py
+++ pyromod/__init__.py
@@ -20,18 +20,16 @@ along with pyromod.  If not, see <https://www.gnu.org/licenses/>.
 
 from .config import config
 from .helpers import ikb, bki, ntb, btn, kb, kbtn, array_chunk, force_reply
-from .listen import Client, MessageHandler, CallbackQueryHandler, Message, Chat, User
+from .listen import Client, Message, Chat, User
 from .nav import Pagination
 from .utils import patch_into, should_patch
 
 __all__ = [
     "config",
     "Client",
-    "MessageHandler",
     "Message",
     "Chat",
     "User",
-    "CallbackQueryHandler",
     "patch_into",
     "should_patch",
     "ikb",
