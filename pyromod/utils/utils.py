e3981feab81a60c0a0389df0a69c7e4322d62469
Ｊｕｓｉｄａｍａ Ｂｏｔ <97734754+Jusidama-Bot@users.noreply.github.com>
2023-10-29 08:49:07 +0700

    Update for filters and if from_user is None (#17)
    * Update listen.py
    
    * Update listen.py
    
    * Update utils.py
    
    * Changes to be committed:
    	modified:   pyromod/listen/listen.py
    	modified:   pyromod/utils/utils.py
    
    * Changes to be committed:
    	modified:   pyromod/utils/utils.py
    
    * Changes to be committed:
    	modified:   pyromod/utils/utils.py
    
    * Changes to be committed:
    	modified:   pyromod/utils/utils.py
    
    * Update utils.py
    
    * Update utils.py
    
    * Update utils.py
    
    * Update utils.py
    
    * Update utils.py
    
    * Update utils.py
    
    * Update utils.py
    
    * Update utils.py
    
    * Changes to be committed:
    	modified:   pyromod/listen/listen.py
    
    * Update pyproject.toml
    
    * Update poetry.lock
    
    * Update poetry.lock
    
    * Update pyproject.toml
    
    * Update listen.py
    
    * Update listen.py
    
    ---------
    
    Co-authored-by: Cezar H <pauxiscezar@gmail.com>

--- pyromod/utils/utils.py
+++ pyromod/utils/utils.py
@@ -17,7 +17,14 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with pyromod.  If not, see <https://www.gnu.org/licenses/>.
 """
+from typing import Callable
+from logging import getLogger
+from inspect import iscoroutinefunction
+from contextlib import contextmanager, asynccontextmanager
 
+from pyrogram.sync import async_to_sync
+
+logger = getLogger(__name__)
 
 class PyromodConfig:
     timeout_handler = None
@@ -31,18 +38,81 @@ class PyromodConfig:
 
 def patch(obj):
     def is_patchable(item):
+        # item = (name, value)
+        # item[0] = name
+        # item[1] = value
         return getattr(item[1], "patchable", False)
 
     def wrapper(container):
         for name, func in filter(is_patchable, container.__dict__.items()):
             old = getattr(obj, name, None)
-            setattr(obj, "old" + name, old)
+            if old is not None: # Not adding 'old' to new func
+                setattr(obj, "old" + name, old)
+            
+            # Worse Code
+            tempConf = {i: getattr(func, i, False) for i in ["is_property", "is_static", "is_context"]}
+            
+            async_to_sync(container, name)
+            func = getattr(container, name)
+            
+            for tKey, tValue in tempConf.items():
+                setattr(func, tKey, tValue)
+
+            if func.is_property:
+                func = property(func)
+            elif func.is_static:
+                func = staticmethod(func)
+            elif func.is_context:
+                if iscoroutinefunction(func.__call__):
+                    func = asynccontextmanager(func)
+                else:
+                    func = contextmanager(func)
+            
+            logger.info(f"Patch Attribute To {obj.__name__} From {container.__name__} : {name}")
             setattr(obj, name, func)
         return container
 
     return wrapper
 
 
-def patchable(func):
-    func.patchable = True
-    return func
+def patchable(is_property: bool = False, is_static: bool = False, is_context: bool = False) -> Callable:
+    """
+    A decorator that marks a function as patchable.
+
+    Usage:
+    
+        @patchable(is_property=True)
+        def my_property():
+            ...
+
+        @patchable(is_static=True)
+        def my_static_method():
+            ...
+
+        @patchable(is_context=True)
+        def my_context_manager():
+            ...
+       
+        @patchable(is_property=False, is_static=False, is_context=False)
+        def my_function():
+            ...
+        
+        @patchable()
+        def default_usage():
+            ...
+
+    Parameters:
+        - is_property (bool): whether the function is a property. Default is False.
+        - is_static (bool): whether the function is a static method. Default is False.
+        - is_context (bool): whether the function is a context manager. Default is False.
+
+    Returns:
+        - A callable object that marks the function as patchable.
+    """
+    def wrapper(func: Callable) -> Callable:
+        func.patchable = True
+        func.is_property = is_property
+        func.is_static = is_static
+        func.is_context = is_context
+        return func
+    return wrapper
