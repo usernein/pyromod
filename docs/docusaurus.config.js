696a5a2f0bc272e50dbf687fe48c010aefdd71bc
Cezar Pauxis <pauxiscezar@gmail.com>
2024-01-26 12:22:26 -0300

    docs: change edit url

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -46,7 +46,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/usernein/pyromod/tree/master/docs/',
         },
         blog: false,
         theme: {
